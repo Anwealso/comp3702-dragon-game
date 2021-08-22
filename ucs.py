03bc17600a69d91c8d015880fc05670aedd5ea8e
Anwealso <48880195+Anwealso@users.noreply.github.com>
2021-08-22 18:22:49 +1000

    Got rid of the 'children' field for Nodes

--- ucs.py
+++ ucs.py
@@ -1,5 +1,3 @@
-print("Imported ucs algorithm")
-
 from game_env import GameEnv
 from game_state import GameState
 import time
@@ -40,23 +38,12 @@ class Node():
         for item in game_state.gem_status:
             self.id = self.id + str(item)
 
-        self.children = []
-
     def __repr__(self):
         try: parent_id = self.parent.id
         except:
             parent_id = None
 
         return "<id:{},parent_id:{},row:{},col:{},gem_status:{},edge_action:{}>".format(self.id, parent_id, self.game_state.row, self.game_state.col, self.game_state.gem_status, self.edge_action)
-
-    def get_child_nodes(self,nodes_list):
-        for child in self.children:
-            if child.children:
-                # If our child has children
-                nodes_list.append(child)
-                child.get_child_nodes(nodes_list)
-            else:
-                nodes_list.append(child)
 
     def get_successors(self, game_env):
         # TODO: Test this function
@@ -102,34 +89,9 @@ class Tree():
         all_nodes = self.explored + self.unexplored
         print('Tree Size:' + str(len(all_nodes)), '(Unexplored:' + str(len(self.unexplored)), ', Explored:' + str(len(self.explored))+")")
 
-    # def mark_explored(self, current_node):
-    #     # Marks the current_node we have just explored as explored
-    #     # print("Marking explored: {}".format(current_node))
-    #     # Add the node to the explored list
-    #     self.explored.append(current_node)
-    #     # Remove the current_node from the unexplored list
-    #     for node in self.unexplored:
-    #         if node.id == current_node.id:
-    #             # If we find a node with the same id in the unexplored list (we 
-    #             # should always find exactly one), then remove it from the list
-    #             self.unexplored.remove(node)
-
-    def add_node(self, parent, node):
-        # Adds an unexplored node to the tree given a node object and a parent node object
-        node.parent = parent
-        parent.children.append(node)
-        self.unexplored.append(node)
-
-    def add_nodes(self, parent, nodes):
-        # Adds a number of nodes all with the same parent
-        for node in nodes:
-            node.parent = parent
-            parent.children.append(node)
-            self.unexplored.append(node)
-
     def get_matching_node(self, current_node):
         """ 
-        Checks whether the tree already contains a node with the same gamestate.
+        Checks whether the tree already contains a node with the same gamestate, and returns it if there is.
 
         Returns:
             the matching node -  if there is a node with same game_state
