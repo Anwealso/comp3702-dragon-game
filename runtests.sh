5b90d0240b77e2e13b4d5ede1ef0f6114bb2146b
Anwealso <48880195+Anwealso@users.noreply.github.com>
2021-08-22 15:39:30 +1000

    Added ability to run single test at a time with commandline arg

--- runtests.sh
+++ runtests.sh
@@ -2,15 +2,16 @@
 passed=0
 failed=0
 
-for i in 1 2 3 4 5 6 7
-do
-    echo "UCS Test $i:"
+if [ $1 ]
+then
+    # If user inputs a specific test no., then run that
+    echo "UCS Test $1:"
     # Run the solution script
-    python3 solution.py testcases/L$i.txt temp.txt ucs;
+    python3 solution.py testcases/L$1.txt temp.txt ucs;
     # Preview the temp file
     # cat temp.txt
     # Run the tests
-    python3 tester.py testcases/L$i.txt temp.txt
+    python3 tester.py testcases/L$1.txt temp.txt
     # Save the output status
     echo $? > temp.txt
     exitcode=$(cat temp.txt)
@@ -31,6 +32,37 @@ do
     # Insert a space after each test
     echo ""
 
-done
+else
+    for i in 1 2 3 4 5 6 7
+    do
+        echo "UCS Test $i:"
+        # Run the solution script
+        python3 solution.py testcases/L$i.txt temp.txt ucs;
+        # Preview the temp file
+        # cat temp.txt
+        # Run the tests
+        python3 tester.py testcases/L$i.txt temp.txt
+        # Save the output status
+        echo $? > temp.txt
+        exitcode=$(cat temp.txt)
+        echo "[[ Exit status: $exitcode ]]"
+
+        # If exit code == 0, then passed, otherwise failed
+        if [ $exitcode -eq 0 ]
+        then
+            # code 0 = pass
+            ((passed++))
+        else
+            # codes -1, 255 = fail
+            ((failed++))
+        fi
+
+        # Delete the temp file
+        rm -f temp.txt
+        # Insert a space after each test
+        echo ""
+
+    done
+fi
 
 echo "Passed: $passed, Failed: $failed"
